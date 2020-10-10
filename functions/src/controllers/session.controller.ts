ab1c223ee92a04b3c6d13743e532798d885f328f
Miguel Rodriguez <miguelantonio_rodriguez@ucol.mx>
2020-10-09 21:27:28 -0500

    saving changes for deploy

--- functions/src/controllers/session.controller.ts
+++ functions/src/controllers/session.controller.ts
@@ -2,7 +2,6 @@ import { Response, Request } from 'express';
 import { keyFilename } from '../index';
 import { SessionsClient } from '@google-cloud/dialogflow';
 import asyncHandler from '../helpers/asyncHandler';
-import { WebhookClient } from 'dialogflow-fulfillment';
 import { v4 as uuidv4 } from 'uuid';
 
 
@@ -27,19 +26,19 @@ export default class SessionController {
             }
         }
         // console.log(request);
-        await sessionClient.detectIntent(request);
+       await sessionClient.detectIntent(request);
         
-        const agent = new WebhookClient({ request: req, response: res });
-        // console.log('body: ', req.body);
+        // const agent = new WebhookClient({ request: req, response: res });
+        // // console.log('body: ', req.body);
     
-        console.log({
-            intent: agent.intent,
-            Query: agent.query,
-            requesSource: agent.requestSource,
-            session: agent.session,
-            consoleMessages: agent.consoleMessages
-        });
-        // console.log(response);
+        // console.log({
+        //     intent: agent.intent,
+        //     Query: agent.query,
+        //     requesSource: agent.requestSource,
+        //     session: agent.session,
+        //     consoleMessages: agent.consoleMessages
+        // });
+        // // console.log(response);
     })
 
     public async detectIntent( req: Request, res: Response ): Promise<void> {
@@ -65,7 +64,7 @@ export default class SessionController {
             // console.log("Antes de llegar a la asginacion: ", req.body);
             req.body = { ...response[0], ...req.body, session: sessionPath};
             console.log("despues de llegar a la asginacion: ", req.body);
-            const agent = new WebhookClient({ request: req, response: res });
+            // const agent = new WebhookClient({ request: req, response: res });
             
             // const cuerpo = [{
             //     responseId: 'a1d88574-918b-4aae-bf43-5c1e6369ac5b-d794dba9',
@@ -74,13 +73,13 @@ export default class SessionController {
             //     outputAudio: "",
             //     outputAudioConfig: ""
             // }];
-            console.log({
-                intent: agent.intent,
-                Query: agent.query,
-                requesSource: agent.requestSource,
-                session: agent.session,
-                consoleMessages: agent.consoleMessages
-            });
+            // console.log({
+            //     intent: agent.intent,
+            //     Query: agent.query,
+            //     requesSource: agent.requestSource,
+            //     session: agent.session,
+            //     consoleMessages: agent.consoleMessages
+            // });
         
 
         } catch (error) {
